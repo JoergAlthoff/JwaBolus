4eb6af4987986f2af533a771a63c86e669f88f4c
joorgalthoff <joerg.althoff@gmx.de>
2024-05-26 23:54:40 +0200

    Initial Commit

---
+++ JwaBolus/JwaBolusApp.swift
@@ -0,0 +1,17 @@
+//
+//  JwaBolusApp.swift
+//  JwaBolus
+//
+//  Created by Jörg Althoff on 26.05.24.
+//
+
+import SwiftUI
+
+@main
+struct JwaBolusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
