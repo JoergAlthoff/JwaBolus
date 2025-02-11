259535233bc5bb2bd5b9e1c4dfac048f6dffba9c
GitJoerg <frost.telexe.4o@icloud.com>
2025-02-11 20:44:53 +0100

    Ändert Text

--- JwaBolus/ContentView.swift
+++ JwaBolus/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hallo, Jörg!")
+                
         }
         .padding()
     }
