b197fece6044afb85279844c6ac48b994ec0bfd7
GitJoerg <frost.telexe.4o@icloud.com>
2025-02-23 22:46:01 +0100

    Anpassung des Models auf die 4 Tageszeiten

--- JwaBolus/BolusViewModel.swift
+++ JwaBolus/BolusViewModel.swift
@@ -1,21 +1,14 @@
-//
-//  BolusViewModel.swift
-//  JwaBolus
-//
-//  Created by Jörg Althoff on 14.02.25.
-//
 import SwiftUI
 
 class BolusViewModel: ObservableObject {
     @Published var aktuellerBZ: Int = 110
     @Published var kohlenhydrate: Int = 0
     @Published var gesamtIE: Double? = nil
-
-
+    @Published var ergebnisseProTageszeit: [TimePeriod: Double] = [:]
 
     @AppStorage("mahlzeitenInsulin") private var mahlzeitenInsulin: Double = 3.5
     @AppStorage("korrekturFaktor") private var korrekturFaktor: Int = 20
-   
+    
     @AppStorage("zielBZ") private var gespeicherterZielBZ: Int = 110
     var zielBZ: Int {
         gespeicherterZielBZ
@@ -38,24 +31,28 @@ class BolusViewModel: ObservableObject {
             letzteInsulinZeitString = ISO8601DateFormatter().string(from: newValue)
         }
     }
-
-    
-
     
     func berechneIE() {
         // Tastatur schließen
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         
         // Eingaben validieren
-        let bz = aktuellerBZ >= 0 ? aktuellerBZ : 0
-        let kh = kohlenhydrate >= 0 ? kohlenhydrate : 0
+        let bz = Double(aktuellerBZ >= 0 ? aktuellerBZ : 0)
+        let kh = Double(kohlenhydrate >= 0 ? kohlenhydrate : 0)
         
-        // Berechnung der Mahlzeiten-Insulinmenge
-        let bolusIE = kh > 0 ? (Double(kh) / 10.0 * mahlzeitenInsulin) : 0.0
+        var ergebnisse: [TimePeriod: Double] = [:]
         
-        // Berechnung der Korrektur-Insulinmenge
-        let korrekturIE = Double(bz - zielBZ) / Double(korrekturFaktor)  // Negativ erlaubt
-        gesamtIE = bolusIE + korrekturIE
+        for period in TimePeriod.allCases {
+            let zielBZ = Double(loadTimeSettings()[period]?.targetBZ ?? 110)
+            let korrekturFaktor = Double(loadTimeSettings()[period]?.correctionFactor ?? 20)
+            let mahlzeitenInsulin = loadTimeSettings()[period]?.mealInsulinFactor ?? 1.0
+            
+            let bolusIE = kh > 0 ? (kh / 10.0 * mahlzeitenInsulin) : 0.0
+            let korrekturIE = (bz - zielBZ) / korrekturFaktor
+            ergebnisse[period] = bolusIE + korrekturIE
+        }
+        
+        ergebnisseProTageszeit = ergebnisse
     }
     
     func minutenSeitLetzterInsulingabe() -> Int {
@@ -68,7 +65,4 @@ class BolusViewModel: ObservableObject {
         let minuten = minutenSeitLetzterInsulingabe()
         return minuten / 30
     }
-    
-
-
 }
